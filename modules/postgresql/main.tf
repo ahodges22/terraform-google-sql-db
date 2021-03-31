4420c53a4b3b2dc079d08490755f3d7760052fd6
Alex Hodges <ahodges22@users.noreply.github.com>
2021-03-30 21:37:34 -0700

    fix: ignore insights config

--- modules/postgresql/main.tf
+++ modules/postgresql/main.tf
@@ -101,7 +101,8 @@ resource "google_sql_database_instance" "default" {
 
   lifecycle {
     ignore_changes = [
-      settings[0].disk_size
+      settings[0].disk_size,
+      settings.insights_config
     ]
   }
 
